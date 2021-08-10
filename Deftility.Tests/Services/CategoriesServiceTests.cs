fe11982ea276b4ee08a5824c67e9f6a3d97bbdc7
Vanka7a <ivanzdravchev@outlook.com>
2021-08-10 12:34:49 +0300

    Added tests for category and skill services

---
+++ Deftility.Tests/Services/CategoriesServiceTests.cs
@@ -0,0 +1,113 @@
+﻿using Deftility.Data;
+using Deftility.Data.Models;
+using Deftility.Data.Repository;
+using Deftility.Services;
+using Deftility.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Deftility.Tests.Services
+{
+    public class CategoriesServiceTests
+    {
+        private readonly IRepository<Category> categoriesRepository;
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            this.categoriesRepository = new EfRepository<Category>(dbContext);
+            this.categoriesService = new CategoriesService(categoriesRepository);
+        }
+
+        [Fact]
+        public void AllReturnsEmptyCollection()
+        {
+            var allCategories = categoriesService.All().ToList();
+            Assert.Empty(allCategories);
+        }
+
+        [Fact]
+        public async Task AddAsyncAddsNewCategorySuccessfully()
+        {
+            await categoriesService.AddAsync("Programming");
+
+            Assert.Single(categoriesService.All());
+            Assert.Equal("Programming", categoriesService.All().First().Name);
+        }
+
+        [Fact]
+        public async Task AddRangeAsyncAddsNewCategoriesSuccessfully()
+        {
+            await categoriesService.AddRangeAsync(new string[] { "C#", "React" });
+
+            Assert.Equal(2, categoriesService.All().Count());
+            Assert.NotNull(categoriesService.All().SingleOrDefault(c => c.Name == "C#"));
+            Assert.NotNull(categoriesService.All().SingleOrDefault(c => c.Name == "React"));
+        }
+
+        [Fact]
+        public async Task RemoveAsyncRemovesCategorySuccessfully()
+        {
+            await categoriesService.AddAsync("Programming");
+
+            Assert.Single(categoriesService.All());
+
+            var category = categoriesRepository.All().First();
+
+            Assert.Equal("Programming", category.Name);
+
+            await categoriesService.RemoveAsync(category);
+
+            Assert.Empty(categoriesService.All());
+        }
+
+        [Fact]
+        public void FindReturnsNullWhenNotFound()
+        {
+            var category = categoriesService.Find("MissingId");
+
+            Assert.Null(category);
+        }
+
+        [Fact]
+        public async Task FindReturnsCategoryWhenFound()
+        {
+            await categoriesService.AddAsync("Programming");
+
+            var categoryDto = categoriesService.All().First();
+
+            var category = categoriesService.Find(categoryDto.Id);
+
+            Assert.IsType<Category>(category);
+            Assert.Equal("Programming", category.Name);
+        }
+
+        [Fact]
+        public void ExistsReturnsFalseWhenNotFound()
+        {
+            var categoryExists = categoriesService.Exists("MissingId");
+
+            Assert.False(categoryExists);
+        }
+
+        [Fact]
+        public async Task ExistsReturnsTrueWhenFound()
+        {
+            await categoriesService.AddAsync("Programming");
+
+            var categoryDto = categoriesService.All().First();
+
+            var categoryExists = categoriesService.Exists(categoryDto.Id);
+
+            Assert.True(categoryExists);
+        }
+    }
+}
---
+++ Deftility.Tests/Services/SkillsServiceTests.cs
@@ -0,0 +1,113 @@
+﻿using Deftility.Data;
+using Deftility.Data.Models;
+using Deftility.Data.Repository;
+using Deftility.Services;
+using Deftility.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Deftility.Tests.Services
+{
+    public class SkillsServiceTests
+    {
+        private readonly IRepository<Skill> skillsRepository;
+        private readonly ISkillsService skillsService;
+
+        public SkillsServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            this.skillsRepository = new EfRepository<Skill>(dbContext);
+            this.skillsService = new SkillsService(skillsRepository);
+        }
+
+        [Fact]
+        public void AllReturnsEmptyCollection()
+        {
+            var allSkills = skillsService.All().ToList();
+            Assert.Empty(allSkills);
+        }
+
+        [Fact]
+        public async Task AddAsyncAddsNewSkillSuccessfully()
+        {
+            await skillsService.AddAsync("HTML");
+
+            Assert.Single(skillsService.All());
+            Assert.Equal("HTML", skillsService.All().First().Name);
+        }
+
+        [Fact]
+        public async Task AddRangeAsyncAddsNewSkillsSuccessfully()
+        {
+            await skillsService.AddRangeAsync(new string[] { "HTML", "CSS" });
+
+            Assert.Equal(2, skillsService.All().Count());
+            Assert.NotNull(skillsService.All().SingleOrDefault(s => s.Name == "HTML"));
+            Assert.NotNull(skillsService.All().SingleOrDefault(s => s.Name == "CSS"));
+        }
+
+        [Fact]
+        public async Task RemoveAsyncRemovesSkillSuccessfully()
+        {
+            await skillsService.AddAsync("HTML");
+
+            Assert.Single(skillsService.All());
+
+            var skill = skillsRepository.All().First();
+
+            Assert.Equal("HTML", skill.Name);
+
+            await skillsService.RemoveAsync(skill);
+
+            Assert.Empty(skillsService.All());
+        }
+
+        [Fact]
+        public void FindReturnsNullWhenNotFound()
+        {
+            var skill = skillsService.Find("MissingId");
+
+            Assert.Null(skill);
+        }
+
+        [Fact]
+        public async Task FindReturnsSkillWhenFound()
+        {
+            await skillsService.AddAsync("HTML");
+
+            var skillDto = skillsService.All().First();
+
+            var skill = skillsService.Find(skillDto.Id);
+
+            Assert.IsType<Skill>(skill);
+            Assert.Equal("HTML", skill.Name);
+        }
+
+        [Fact]
+        public void ExistsReturnsFalseWhenNotFound()
+        {
+            var skillExists = skillsService.Exists("MissingId");
+
+            Assert.False(skillExists);
+        }
+
+        [Fact]
+        public async Task ExistsReturnsTrueWhenFound()
+        {
+            await skillsService.AddAsync("HTML");
+
+            var skillDto = skillsService.All().First();
+
+            var skillExists = skillsService.Exists(skillDto.Id);
+
+            Assert.True(skillExists);
+        }
+    }
+}
